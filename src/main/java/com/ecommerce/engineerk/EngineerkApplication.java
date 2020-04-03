8423223f86fe4d54cadac1b3bcca5dcfda9e4d02
erkan obut <tbd.eobut@softtech.com.tr>
2020-04-04 00:26:50 +0300

    first service commit

---
+++ src/main/java/com/ecommerce/engineerk/EngineerkApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.engineerk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineerkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineerkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/engineerk/EngineerkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.engineerk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineerkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
